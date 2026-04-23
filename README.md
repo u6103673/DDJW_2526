44d590dbee457db690d5086e6004bcea64683f65
u6103673 <u6103673@campus.udg.edu>
2026-04-23 21:28:31 +0200

    Resolució de conflictes i creació del readme

---
+++ README.md
@@ -0,0 +1,10 @@
+#Treball individual - Memory
+#Júlia Conde Fabra - u6103673
+
+#Introducció
+
+#Descripció del disseny de joc
+
+#Descripció de les parts més rellevants de la implementació
+
+#Conclusions i problemes trobats
--- js/main.js
+++ js/main.js
@@ -4,15 +4,34 @@ $(document).ready(function() {
     $('#play').click(function(){
         let alies = prompt("Introdueix l'àlies:");
         console.log("L'àlies és: " + alies);
+        sessionStorage.removeItem('load');
         window.location.assign("./html/game.html");
     });
 
     $('#options').click(function(){
-        console.error("Opció no implementada");
+        window.location.assign("./html/options.html");
     });
 
     $('#saves').click(function(){
-        console.error("Opció no implementada");
+        let to_load = localStorage.save;
+        fetch('../php/load.php', {
+            method: "POST",
+            body: JSON.stringify({}),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+        .then(response => response.json())
+        .then(json => to_load = (!json.error)?JSON.stringify(json.save): localStorage.save)
+        .catch (err => {
+            console.error(err);
+            console.warn("La partida s'intentarà carregar de local");
+        });
+
+        if (!to_load) {
+            alert("No hi ha cap partida a carregar");
+            return;
+        }
+        sessionStorage.load = to_load;
+        window.location.assign("./html/game.html");
     });
 
     $('#exit').click(function(){
