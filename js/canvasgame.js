68b08881f793699187d550bbde82ab4f57910ad6
u6103673 <u6103673@campus.udg.edu>
2026-06-15 20:39:11 +0200

    Canvi del joc a CANVAS

--- js/canvasgame.js
+++ js/canvasgame.js
@@ -4,14 +4,15 @@ import {clickCard, gameItems, selectCards, startGame, initCard, saveGame} from "
 let game = $('#game');
 let canvas = game[0].getContext('2d');
 let resources = {};
-let cards = {};
-const e_click = {click: false, x: -1, y: -1}
+let cards = [];
+const e_click = {click: false, x: -1, y: -1};
 let key = null;
 const c_w = 96;
 const c_h = 128;
 let idxSel = -1;
 
 if (canvas){
+    //mida del canvas
     game.attr("width", 800);
     game.attr("height", 600);
     start();
@@ -21,65 +22,92 @@ if (canvas){
 function start(){
     selectCards();
     cards = gameItems.map((c)=>{return {texture:c}});
-    loadCardResource("../resources/back.png");
+
+    loadCardResource("../resources/back.svg"); 
+    
+    let columns = 4;
+    if (cards.length === 6) columns = 3;
+    else if (cards.length === 8) columns = 4;
+    else if (cards.length === 10) columns = 5;
+    else if (cards.length === 12) columns = 4;
+    else if (cards.length > 12) columns = 6;
+
+    const rows = Math.ceil(cards.length / columns);
+    const padding = 20; // Espai entre cartes
+    const gridWidth = (columns * c_w) + ((columns - 1) * padding);
+    const gridHeight = (rows * c_h) + ((rows - 1) * padding);
+    const gridXStart = (800 - gridWidth) / 2;
+    const gridYStart = (600 - gridHeight) / 2;
+
     cards.forEach((card, indx) => {
         loadCardResource(card.texture);
         initCard(val => card.texture = val);
+
+        const col = indx % columns;              
+        const row = Math.floor(indx / columns);  
+
         card.position = {
-            xMin: 2+100*indx,
-            xMax: 2+100*indx + c_w,
-            yMin: 0,
-            yMax: c_h
-        }
+            xMin: gridXStart + (c_w + padding) * col,
+            yMin: gridYStart + (c_h + padding) * row,
+            xMax: 0,
+            yMax: 0
+        };
+        card.position.xMax = card.position.xMin + c_w;
+        card.position.yMax = card.position.yMin + c_h;
+
         card.onClick = function(x, y){
-            return x >= this.position.xMin && x <= this.position.xMax &&
-                    y >= this.position.yMin && y <= this.position.yMax;
-        }
+            return x >= this.position.xMin && x <= this.position.xMax && y >= this.position.yMin && y <= this.position.yMax;
+        };
     });
-    // Vincular events
+    
     game.on('click', function(e){
         e_click.click = true;
         e_click.x = e.pageX - this.offsetLeft;
         e_click.y = e.pageY - this.offsetTop;
     });
-    $(document).keydown(e=>key = e.key);
+    
+    $('#save').on('click', () => saveGame());
+
+    $(document).keydown(e => key = e.key);
+    
     startGame();
 }
 
 function update(){
     checkInput();
     draw();
+    //bucle
     requestAnimationFrame(update);
 }
 
 function loadCardResource(src){
     if (!resources[src]){
-        let res = {image: null, ready: false}
+        let res = {image: null, ready: false};
         res.image = new Image();
         res.image.src = src;
-        res.image.onload = ()=> res.ready = true;
+        res.image.onload = () => res.ready = true;
         resources[src] = res;
     }
 }
 
 function draw(){
-    canvas.reset();
+    canvas.clearRect(0, 0, 800, 600); 
+    
     cards.forEach((card, indx) => {
         let res = resources[card.texture];
-        if (res.ready){
-            if (idxSel === indx)
-                canvas.drawImage(res.image, card.position.xMin, 
-                                card.position.yMin, c_w + 4, c_h + 4);
-            else
-                canvas.drawImage(res.image, card.position.xMin, 
-                                    card.position.yMin, c_w, c_h);
+        if (res && res.ready){
+            if (idxSel === indx) {
+                canvas.drawImage(res.image, card.position.xMin, card.position.yMin, c_w + 4, c_h + 4);
+            } else {
+                canvas.drawImage(res.image, card.position.xMin, card.position.yMin, c_w, c_h);
+            }
         }
     });
 }
 
 function checkInput(){
     if (e_click.click){
-        cards.some((card, indx)=>{
+        cards.some((card, indx) => {
             let click = card.onClick(e_click.x, e_click.y);
             if (click) clickCard(indx);
             return click;
@@ -92,24 +120,28 @@ function checkInput(){
                 saveGame();
                 break;
             case "ArrowRight":
-                idxSel = (idxSel + 1)%cards.length;
+                idxSel = (idxSel + 1) % cards.length;
                 break;
             case "ArrowLeft":
-                idxSel = (idxSel - 1 + cards.length)%cards.length;
+                idxSel = (idxSel - 1 + cards.length) % cards.length;
                 break;
             case "Enter":
                 if (idxSel >= 0) clickCard(idxSel);
                 break;
             default:
-                console.warn("Tecla "+key+" no reconeguda.");
+                console.warn("Tecla " + key + " no reconeguda.");
         }
-        if (idxSel != prevIndx){
+        
+        if (idxSel !== prevIndx){
             if (prevIndx >= 0) {
                 cards[prevIndx].position.xMin += 2;
             }
-            cards[idxSel].position.xMin -= 2;
+            if (idxSel >= 0) {
+                cards[idxSel].position.xMin -= 2;
+            }
         }
     }
-    e_click.click = key = false;
+    
+    e_click.click = false;
+    key = null;
 }
-
